607178978747f9e2b87993294957a4a0ec572830
Noman Maniyar <46672511+nomanmaniyar@users.noreply.github.com>
2023-12-18 14:06:53 +0530

    expenditure assigment completed

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -4,17 +4,33 @@
   transactions is an array where each
   Transaction - an object like 
         {
-		id: 1,
-		timestamp: 1656076800000,
-		price: 10,
-		category: 'Food',
-		itemName: 'Pizza',
-	}
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  }
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const categories = [];
+  const result = [];
+  for (let index = 0; index < transactions.length; index++) {
+    const cat = transactions[index][category];
+    categories.push(cat);
+  }
+  for (let index = 0; index < categories.length; index++) {
+    const total = 0;
+    transactions.array.forEach((transaction) => {
+      if (transaction["category"] === categories[index]) {
+        total += transaction["price"];
+      }
+    });
+    result.push(new Object({ category: categories[index], price: total }));
+  }
+
+  return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
