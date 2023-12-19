8e374c9a1af9416015399f2a3f25de636d190106
Noman Maniyar <46672511+nomanmaniyar@users.noreply.github.com>
2023-12-19 12:06:19 +0530

    findLargestElement assignment completed

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -1,12 +1,18 @@
 /*
-  Write a function `findLargestElement` that takes an array of numbers and returns the largest element.
-  Example:
-  - Input: [3, 7, 2, 9, 1]
-  - Output: 9
+  Write a function `findLargestElement` that takes an array of numbers and returns the largest element.
+  Example:
+  - Input: [3, 7, 2, 9, 1]
+  - Output: 9
 */
 
 function findLargestElement(numbers) {
-    
+    let largest = numbers[0];
+    for (let index = 1; index < numbers.length; index++) {
+        if (numbers[index] > largest) {
+            largest = numbers[index];
+        }
+    }
+    return largest;
 }
 
 module.exports = findLargestElement;
