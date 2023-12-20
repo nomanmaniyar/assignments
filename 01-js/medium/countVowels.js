82ba3d19143caa5363e738ffe0a455866b942f3a
Noman Maniyar <46672511+nomanmaniyar@users.noreply.github.com>
2023-12-20 12:13:42 +0530

    countVowel assignment Completed

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,23 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  // Your code here
+  const arr = str.toLowerCase().split("");
+  let count = 0;
+  arr.forEach((item) => {
+    if (
+      item === "a" ||
+      item === "e" ||
+      item === "i" ||
+      item === "o" ||
+      item === "u"
+    ) {
+      count++;
+    }
+
+  });
+  console.log("COUNT");
+  return count;
 }
 
 module.exports = countVowels;
